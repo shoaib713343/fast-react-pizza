9720996d3b2ac1329e9517d3dcc1b550abe23a4d
Shoaib <shoaib713343@gmail.com>
2025-08-21 15:16:40 +0530

    Adding menu items to the cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,22 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import {addItem} from "../cart/cartSlice"
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart(){
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem))
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -13,7 +27,9 @@ function MenuItem({ pizza }) {
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : 
           <p className="text-sm font-medium uppercase text-stone-500">Sold out</p>}
-          <Button type="small">Add to Cart</Button>
+          {!soldOut && <Button type="small"
+          onClick={handleAddToCart}
+          >Add to Cart</Button>}
         </div>
       </div>
     </li>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 
-function Button({children, disabled, to, type}) {
+function Button({children, disabled, to, type, onClick}) {
   const base =
     'inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
@@ -11,6 +11,15 @@ function Button({children, disabled, to, type}) {
   };
     if(to)
         return <Link className={styles[type]} to={to}>{children}</Link>
+    
+    if (onClick) {
+      return (
+        <button disabled={disabled}
+          className={styles[type]} onClick={onClick}
+          >{children}</button>
+    )
+    }
+
     return (
         <button disabled={disabled}
           className={styles[type]}
