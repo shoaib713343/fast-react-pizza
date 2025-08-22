63d37adf9ec3e6640f50ac21389414e5002c708f
Shoaib <shoaib713343@gmail.com>
2025-08-22 15:09:17 +0530

    updating cart quantities

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -20,7 +20,7 @@ function Cart() {
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
 <ul className="divide-y divide-stone-200 border-b mt-3">
-  {cart.map(item=> <CartItem item={item} key={item.key} />)}
+  {cart.map(item=> <CartItem item={item} key={item.pizzaId} />)}
 </ul>
 
       <div className="mt-6 space-x-2">
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -34,6 +34,7 @@ const cartSlice = createSlice({
         pizzaId === action.payload);
         item.quantity--;
         item.totalPrice = item.quantity * item.unitPrice;
+        if(item.quantity ===0) cartSlice.caseReducers.deleteItem(state, action);
         },
         clearCart(state){
             state.cart = []
@@ -47,4 +48,6 @@ export const getCart = (state) => state.cart.cart;
 
 export const getTotalCartQuantity = state => state.cart.cart.reduce((sum, item)=>sum+item.quantity,0);
 export const getTotalCartPrice = state => state.cart.cart.reduce((sum, item)=>sum+item.totalPrice,0)
+
+export const getCurrentQuantityById = id => state => state.cart.cart.find(item => item.pizzaId === id)?. quantity ?? 0;
 
