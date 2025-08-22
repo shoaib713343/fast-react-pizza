eb5ec0cb20c7c9043588a1138a13f12c84c79214
Shoaib <shoaib713343@gmail.com>
2025-08-22 10:25:34 +0530

    Bulding the cart page

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,12 +1,19 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const TotalCartPrice = useSelector(getTotalCartPrice);
+
+  if(!totalCartQuantity) return null;
+  
   return (
     <div className="bg-stone-800 text-stone-200 uppercase py-4 px-4 sm:px-6
     text-sm md:text-base flex items-center justify-between">
       <p className="text-stone-300 font-semibold space-x-4 sm:x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>${TotalCartPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
